85753fb20008f1a8e0d523f2cb297a708a76e29b
Muhammad Rony <ha2ronyahamed@gmail.com>
2025-10-27 23:57:54 +0600

    ekattor shop

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 import { Link, NavLink } from "react-router-dom";
+import { X } from "lucide-react";
+import { FaFacebookF, FaPinterestP } from "react-icons/fa";
 
 const MobileMenu = ({ menuOpen, closeMenu }) => {
   return (
@@ -13,20 +15,7 @@ const MobileMenu = ({ menuOpen, closeMenu }) => {
           <div className="menu-close">
             <button className="menu-close-btn" onClick={closeMenu}>
               <span className="menu-close-icon">
-                <svg
-                  viewBox="0 0 24 24"
-                  width="24"
-                  height="24"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  fill="none"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  className="css-i6dzq1"
-                >
-                  <line x1="18" y1="6" x2="6" y2="18"></line>
-                  <line x1="6" y1="6" x2="18" y2="18"></line>
-                </svg>
+                <X size={24} strokeWidth={2} />
               </span>
             </button>
           </div>
@@ -311,12 +300,7 @@ const MobileMenu = ({ menuOpen, closeMenu }) => {
                   <li>
                     <Link to="https://www.facebook.com/">
                       <span className="icon-social facebook">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 320 512"
-                        >
-                          <path d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z" />
-                        </svg>
+                        <FaFacebookF size={20} />
                       </span>
 
                       <span>90K Followers</span>
@@ -326,12 +310,7 @@ const MobileMenu = ({ menuOpen, closeMenu }) => {
                   <li>
                     <Link to="https://in.pinterest.com/">
                       <span className="icon-social pinterest">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 448 512"
-                        >
-                          <path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z" />
-                        </svg>
+                        <FaPinterestP size={20} />
                       </span>
 
                       <span>60K Followers</span>
